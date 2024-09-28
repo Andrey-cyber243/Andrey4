469fd84e3b4435388a74d7dcbfa19fb472fb6ed6
Andrey-cyber243 <Sapuntsov64@mail.ru>
2024-09-28 15:23:14 +0300

    Add files via upload

---
+++ mobule_1_5.ru.py
@@ -0,0 +1,7 @@
+immutable_var=1,2,3,True,"time"
+print(immutable_var)
+#immutable_var[0]=250 #Не поддерживает обрашение по элементам.
+#print(immutable_var)
+Mutable_list=['time','main', 'code',1,7,5]
+Mutable_list[0]='code'
+print(Mutable_list)
